981c5401d2ebe7d1392ff1c69643d4388e2ef7ed
jose.gg88 <jose.gg88@b9078852-2b58-11df-b850-2185049e3855>
2010-03-15 17:33:54 +0000

    Servidor corba funcionando, falta crear un cliente de prueba

--- RehearsalReservationCore/src/corbaServer/dao/CorbaOperaHouseDAO.java
+++ RehearsalReservationCore/src/corbaServer/dao/CorbaOperaHouseDAO.java
@@ -1,24 +1,51 @@
 package corbaServer.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import corbaServer.RehearsalDO;
 
 public class CorbaOperaHouseDAO implements ICorbaOperaHouseDAO {
-
-	public void connect() {
+	
+	Connection conn;
+	Statement stat;
+	
+	public void connect() throws ClassNotFoundException, SQLException {
+		
+		Class.forName("org.sqlite.JDBC");
+		conn = DriverManager.getConnection("jdbc:sqlite:db/corba-db/scalaMilano.db");
 
 	}
 
 	public List<RehearsalDO> getRehearsals() throws SQLException {
-		List<RehearsalDO> rehearsals = null;
+		List<RehearsalDO> rehearsals = new ArrayList<RehearsalDO>();
+		String query = "select * from rehearsalst";
+		stat = conn.createStatement();
+		
+		ResultSet rs = stat.executeQuery(query);
+		
+		while(rs.next())
+		{
+			String operaName = rs.getString(1);
+			String date = rs.getString(2) ;
+			int seats = rs.getInt(3);
+			
+			RehearsalDO temp = new RehearsalDO(operaName,date,seats);
+			rehearsals.add(temp);
+		}
+		rs.close();
+		stat.close();
 
 		return rehearsals;
 	}
 
-	public void disconnect() {
-
+	public void disconnect() throws SQLException {
+		conn.close();
 	}
 }
---
+++ RehearsalReservationCore/src/corbaServer/dao/prueba.java
@@ -0,0 +1,46 @@
+package corbaServer.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import corbaServer.RehearsalDO;
+
+public class prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		CorbaOperaHouseDAO base = new CorbaOperaHouseDAO();
+		List <RehearsalDO> lista = new ArrayList<RehearsalDO>();
+		
+		try 
+		{
+			base.connect();
+			lista = base.getRehearsals();
+			
+			for(int i = 0; i< lista.size();i++)
+			{
+				System.out.println(lista.get(i).getOperaName() + lista.get(i).getDate() + lista.get(i).getSeats());
+				
+			}
+			
+			base.disconnect();
+			
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
