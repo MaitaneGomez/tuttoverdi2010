00be0f7629e89cd023c4c40673c20f0b41eb10a4
jose.gg88 <jose.gg88@b9078852-2b58-11df-b850-2185049e3855>
2010-03-20 19:02:51 +0000

    Acabado y probado con una calse de prueba, la creacion de la cache. NOTA: en las bases de datos, los datos introducidos son case sensitive

--- trunk/RehearsalReservationCore/src/rehearsalServer/OperaRehearsalServer.java
+++ trunk/RehearsalReservationCore/src/rehearsalServer/OperaRehearsalServer.java
@@ -23,13 +23,12 @@ import util.observer.rmi.RemoteObservable;
 
 public class OperaRehearsalServer extends UnicastRemoteObject implements IOperaRehearsalServer{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	@SuppressWarnings("unused")
 	private util.observer.rmi.RemoteObservable remoteObservable;
 	private Map<String, Map<String, RehearsalRMIDTO>> rehearsalCache;
-	
+
+
 	/**
 	 * CACHE OF RehearsalRMIDTO objects, organized by Opera House Name To be
 	 * loaded at the initialization process
@@ -43,19 +42,37 @@ public class OperaRehearsalServer extends UnicastRemoteObject implements IOperaR
 		rehearsalCache = createCache();
 	}
 	
+	
 	private Map<String, Map<String, RehearsalRMIDTO>> createCache()
 	{
 		CorbaOperaHouseDAO dataBase = new CorbaOperaHouseDAO();
-		List <RehearsalDO> lista = new ArrayList<RehearsalDO>();
+		List <RehearsalDO> rehearsalDOList = new ArrayList<RehearsalDO>();
+		Map<String, Map<String, RehearsalRMIDTO>> rehearsalsCache = new TreeMap<String, Map<String,RehearsalRMIDTO>>();
 		
 		try 
 		{
 			dataBase.connect("scalaMilano");
-			lista = dataBase.getRehearsals();
-			//falta la lista de scalaNapoli
+			rehearsalDOList = dataBase.getRehearsals();
+			//falta la lista de scalaNapoli para la segunda entrega
 			dataBase.disconnect();
 			
-			//ahora habria que coger las reservas
+			//ahora habria que coger las reservas y obtener sus asientos ocupados
+			
+			//conectamos la base de datos de reservas
+			ReservationCounter.connect();
+			
+			//primero recorremos para scalaMilano y nos creamos su map interno
+			Map<String, RehearsalRMIDTO> scalaMilanoMAP = new TreeMap<String, RehearsalRMIDTO>();
+			for(int i = 0; i<rehearsalDOList.size(); i++)
+			{
+				RehearsalDO x = rehearsalDOList.get(i);
+				int ocupiedSeats = ReservationCounter.obtainOcupiedSeats("ScalaMILANO", x.getOperaName());
+				RehearsalRMIDTO newRehearsalRMIDTO = new RehearsalRMIDTO("ScalaMILANO", x.getOperaName(),x.getDate(),x.getSeats()-ocupiedSeats);
+				scalaMilanoMAP.put(x.getOperaName(), newRehearsalRMIDTO);
+			}
+			ReservationCounter.disconnect();
+			rehearsalsCache.put("ScalaMilano", scalaMilanoMAP);
+			
 			
 		} 
 		catch (ClassNotFoundException e) 
@@ -68,9 +85,16 @@ public class OperaRehearsalServer extends UnicastRemoteObject implements IOperaR
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return rehearsalsCache;
+	}
+	
+	public Map<String, Map<String, RehearsalRMIDTO>> getRehearsalCache() 
+	{
 		return rehearsalCache;
 	}
 
+	
+	
 	public static void main(String[] args) {
 
 	}
