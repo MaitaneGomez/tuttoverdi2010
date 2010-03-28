d6aa5c53d41cc93cf35aa772caf2a3520061ff06
jose.gg88 <jose.gg88@b9078852-2b58-11df-b850-2185049e3855>
2010-03-28 17:46:19 +0000

    funciona lo anterior, pero he tocado lo de authorization y los buil.xml (preguntar a ipiña porque puñetas da un error de rebind)

--- RehearsalReservationCore/src/rehearsalServer/loginGateway/AuthRMIGateway.java
+++ RehearsalReservationCore/src/rehearsalServer/loginGateway/AuthRMIGateway.java
@@ -1,5 +1,14 @@
 package rehearsalServer.loginGateway;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.StringTokenizer;
+
+import authorizationRMI.IAuthorizationRMI;
+import authorizationRMI.InvalidPasswordException;
+import authorizationRMI.InvalidUserException;
+
 public class AuthRMIGateway implements IAuthorizeGateway {
 	/**
 	 * Add your code to invoke the Authorization RMI Server here Remember you
@@ -7,16 +16,55 @@ public class AuthRMIGateway implements IAuthorizeGateway {
 	 * AuthorizationRMIClient.jar THIS SECTION BELONGS TO THE FIRST ASSIGNMENT
 	 */
 	
-	private String server;
+	private String serverName;
+	private String ip;
+	private String port;
 	
 	public AuthRMIGateway(String serviceUri) 
 	{
-		server = serviceUri;
+		StringTokenizer st = new StringTokenizer(serviceUri);
+		ip = st.nextToken();
+		port = st.nextToken();
+		serverName = st.nextToken();
 	}
 
 	public String login(String user, String pass) throws ValidationException 
 	{
 		String studentName = null;
+		String name = "//" + ip + ":" + port + "/" + serverName;
+		try 
+		{
+			IAuthorizationRMI objAuth = (IAuthorizationRMI) java.rmi.Naming.lookup(name);
+			studentName = objAuth.login(user, pass);
+		} 
+		
+		
+		catch (MalformedURLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (RemoteException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (NotBoundException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidUserException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (InvalidPasswordException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 		return studentName;
 	}
