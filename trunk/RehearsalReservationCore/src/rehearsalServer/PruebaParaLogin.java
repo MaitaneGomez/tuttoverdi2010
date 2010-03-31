91a1bbbe106fe5f2327776d3289e2e86f659b1a3
jose.gg88 <jose.gg88@b9078852-2b58-11df-b850-2185049e3855>
2010-03-31 10:30:45 +0000

    funciona el metodo remoto, getRehearsals

--- trunk/RehearsalReservationCore/src/rehearsalServer/PruebaParaLogin.java
+++ trunk/RehearsalReservationCore/src/rehearsalServer/PruebaParaLogin.java
@@ -3,6 +3,7 @@ package rehearsalServer;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.List;
 
 import authorizationRMI.IAuthorizationRMI;
 import authorizationRMI.InvalidPasswordException;
@@ -22,8 +23,14 @@ public class PruebaParaLogin {
 		{
 			IOperaRehearsalServer rehearsalServer = (IOperaRehearsalServer) java.rmi.Naming.lookup(name);
 			System.out.println("he encontrado el objeto rmi remoto...");
-			String studentName = rehearsalServer.login("stud5", "maigo");
+			String studentName = rehearsalServer.login("stud1", "1111");
 			System.out.println("el nombre del estudiante es: " + studentName);
+			
+			List<RehearsalRMIDTO> lista = rehearsalServer.getRehearsals();
+			for(int i=0; i<lista.size();i++)
+			{
+				System.out.println(lista.get(i).getOperaHouse() + " "  + lista.get(i).getOperaName() + " " + lista.get(i).getDate() + " " + lista.get(i).getAvailableSeats());
+			}
 		} 
 		catch (MalformedURLException e) 
 		{
