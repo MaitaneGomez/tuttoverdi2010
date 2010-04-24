278eb8af42950884e4ac4cddf2a348c89338a356
jose.gg88 <jose.gg88@b9078852-2b58-11df-b850-2185049e3855>
2010-04-24 14:53:09 +0000

    El euskalduna esta hecho y probado con un cliente fuera del paquete donde esta el servicio. Falta hacerle el gateway en el core

---
+++ trunk/WebServices/PruebaEuskalduna/src/prueba/EuskaldunaServiceClient.java
@@ -0,0 +1,26 @@
+package prueba;
+
+import proxies.EuskaldunaWebServiceStub;
+import proxies.EuskaldunaWebServiceStub.RehearsalDTO;
+
+public class EuskaldunaServiceClient 
+{
+    public static void main(String[] args) {
+     
+            EuskaldunaWebServiceStub.RehearsalDTO[] array = null;
+
+            try {
+                EuskaldunaWebServiceStub stub = new EuskaldunaWebServiceStub(args[0]);
+               
+
+               
+					array = stub.getRehearsals();
+					for(int i =0; i < array.length; i++)
+						System.out.println(array[i].getOperaName() + " " + array[i].getDate() + " " + array[i].getSeats());
+						//System.out.println(array.length);
+                   
+            } catch (Exception e) {
+                System.err.println("Error ACCESSING the web service: " + e.getMessage());
+            }
+        }
+    }
