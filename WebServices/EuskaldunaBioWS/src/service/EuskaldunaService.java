170d8052ee19dea9a8d679368417026c2bac2163
jose.gg88 <jose.gg88@b9078852-2b58-11df-b850-2185049e3855>
2010-05-11 11:52:51 +0000

    ok antes de meter comentarios

--- WebServices/EuskaldunaBioWS/src/service/EuskaldunaService.java
+++ WebServices/EuskaldunaBioWS/src/service/EuskaldunaService.java
@@ -3,9 +3,12 @@ package service;
 import service.dao.*;
 import service.data.RehearsalDTO;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import javax.naming.NamingException;
 
 
 public class EuskaldunaService {
@@ -14,9 +17,23 @@ public class EuskaldunaService {
 	{
 		RehearsalDTO[] array = null;
 		EuskaldunaBioWSDAO dao = new EuskaldunaBioWSDAO();
-		dao.connect();
-		array = dao.getRehearsals();
-		dao.disconnect();
+		try 
+		{
+			dao.connect();
+			array = dao.getRehearsals();
+			dao.disconnect();
+		} 
+		catch (NamingException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		return array;
 		
